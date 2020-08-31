95a2c370fd5d543faacbd77974ea6f416cce9259
shachindra <shachindra.spidey@gmail.com>
2020-08-31 18:39:27 +0530

    Edited Token Details

--- Token.sol
+++ Token.sol
@@ -4,7 +4,7 @@ import {ERC20} from "./ERC20.sol";
 
 contract NGT is ERC20 {
     function decimals() public pure returns (uint8) {
-        return 10;
+        return 18;
     }
 
     function rounding() public pure returns (uint8) {
@@ -12,7 +12,7 @@ contract NGT is ERC20 {
     }
 
     function name() public pure returns (string memory) {
-        return "Netguard Tokens";
+        return "NetGuard";
     }
 
     function symbol() public pure returns (string memory) {
